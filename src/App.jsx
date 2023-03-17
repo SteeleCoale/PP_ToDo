58261584f9d07997b4461142e6051f996cb05292
LoganBit <loganbit@administrators-MacBook-Pro.local>
2023-03-17 17:31:27 -0400

    fast!

--- src/App.jsx
+++ src/App.jsx
@@ -6,47 +6,43 @@ function App() {
   const [notes, setNotes] = useState([])
 
   const inputRef = useRef()
-  //things I learned
 
-  /** 
-   * arrays are also referencing that to which they are set equal.
-   * so it has to be newNotes = [...notes], not newNotes = notes (this is referring to the same spot in memory)
-   array.splice is modifing the array. So you have to let it do that before setting the modified array into state.
-  */
-  
-  const onAddClickHandler = () => {
-    let newNote = inputRef.current.value
-    let newNotes = [...notes]
-    newNotes.push(newNote)
+  const addButtonClickHandler = () => {
+    const newNotes = [...notes]
+    const newNote = inputRef.current.value
+    if (newNote === '' || newNote === undefined){
+      return
+    } else {
+      newNotes.push(inputRef.current.value)
+    }
     setNotes(newNotes)
     inputRef.current.value = ''
     inputRef.current.focus()
   }
 
-  const removeClickHandler = index => {
-    console.log('remove at index', index)
-    let newNotes = [...notes]
+  const deleteClickHandler = (index) => {
+    const newNotes = [...notes]
     newNotes.splice(index, 1)
     setNotes(newNotes)
-    inputRef.current.focus()
   }
 
   return (
     <>
-      <div className='inputContainer'>
-        <input placeHolder='add note here' ref={inputRef}></input>
-        <button onClick={onAddClickHandler}>Add Note</button>
-      </div>
-      <div className='noteContainer'>
-        {notes.map((note, index) => {
-          return (
-            <div className='note' key={index}>
-            <p>{note}</p>
-            <button onClick={() => removeClickHandler(index)}>Remove note</button>
-            </div>
-          )
-        })}
-      </div>
+    <p>Note taker</p>
+    <div>
+    <input ref={inputRef}></input>
+    <button onClick={addButtonClickHandler}>Add note</button>
+    </div>
+    <ul>
+      {notes.map((note, index) => {
+        return (
+         <div className='note'>
+        <li key={index}>{note}</li>
+        <button onClick={() => deleteClickHandler(index)}>click me</button>
+         </div> 
+        )
+      })}
+    </ul>
     </>
   )
 }
